f61e6f178d6c38b32a7eecc3ffc85e68ccefed41
Armine Aroyan <aroanarmine068@gmail.com>
2025-11-28 22:39:00 +0500

    Task1

--- Tyuiu.AroyanAV.Sprint5.Task1.V19/Program.cs
+++ Tyuiu.AroyanAV.Sprint5.Task1.V19/Program.cs
@@ -1,42 +1,42 @@
-﻿using System;
-using Tyuiu.AroyanAV.Sprint5.Task0.V30.Lib;
+﻿using Tyuiu.AroyanAV.Sprint5.Task1.V19.Lib;
 
-namespace Tyuiu.AroyanAV.Sprint4.Task0.V19
+namespace Tyuiu.AroyanAV.Sprint5.Task1.V19
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            int x = 3;
+            DataServise ds = new DataServise();
 
             Console.Title = "Спринт #5 | Выполнила: Ароян А. В. | ИСТНб-25-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #5                                                               *");
-            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
-            Console.WriteLine("* Задание #0                                                              *");
-            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
             Console.WriteLine("* Выполнила: Ароян Армине Вачагановна | ИСТНб-25-1                        *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в  *");
-            Console.WriteLine("* текстовый файл OutPutFileTask0.txt и вывести на консоль.                *");
-            Console.WriteLine("* Округлить до трёх знаков после запятой.                                 *");
+            Console.WriteLine("* Табулировать функцию F(x) на [-5; 5] с шагом 1 и сохранить в файл       *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            
-            DataService ds = new DataService();
-            Console.WriteLine("x = " + x);
+
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("startValue = " + stopValue);
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            string res = ds.SaveToFileTextData(x);
-
+            string res = ds.SaveToFileTextData(startValue, stopValue);
             Console.WriteLine("Файл: " + res);
             Console.WriteLine("Создан!");
-            Console.ReadLine();
+
+
+            Console.ReadKey();
         }
     }
 }
