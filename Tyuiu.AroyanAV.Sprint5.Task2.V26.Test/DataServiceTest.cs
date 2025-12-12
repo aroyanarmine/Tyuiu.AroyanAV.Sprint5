6cf6f2fa7a6528b1bd4bbcf49eab30b1f0224703
Armine Aroyan <aroanarmine068@gmail.com>
2025-12-13 02:28:42 +0500

    Task2

---
+++ Tyuiu.AroyanAV.Sprint5.Task2.V26.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.AroyanAV.Sprint5.Task2.V26.Lib;
+using System.IO;
+
+namespace Tyuiu.AroyanAV.Sprint5.Task2.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.csv");
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = false;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.AroyanAV.Sprint5.Task2.V26/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Tyuiu.AroyanAV.Sprint5.Task2.V26.Lib;
+
+namespace Tyuiu.AroyanAV.Sprint5.Task2.V26
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[,] mtrx = new int[3, 3] { { 2, -3, -3 },
+                                         {-8, 8, 0 },
+                                         { -6, 1, 0 }};
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.Length / rows;
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Ароян А. В. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнила: Ароян Армине Вачагановна | ИСТНб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры. Заменить положительные элементы массива на 1,  *");
+            Console.WriteLine("* отрицательные на 0.                                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("массив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(mtrx);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+} 
